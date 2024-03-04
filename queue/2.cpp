5ac0ee7f4722832558c84ea6ff44fdf2b992aad6
0xnand <80183902+n3rma121@users.noreply.github.com>
2024-03-04 17:47:02 +0545

    queueSTL.cpp

--- queue/2.cpp
+++ queue/2.cpp
@@ -1,2 +1,33 @@
 // std::queue in cpp
 
+#include <iostream>
+#include <queue>
+
+void showQueue(std::queue<int> q){
+    std::queue<int> temp = q;
+    std::cout << "The queue list is: ";
+    while(!temp.empty()){
+        std::cout << '\t' << temp.front();
+        temp.pop();
+    }
+    std::cout << "\n";
+}
+int main(){
+    std::queue<int> q;
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+
+    std::cout << "Front element: " << q.front() 
+              << "\nRear Element: " << q.back() << std::endl;
+
+    showQueue(q);
+
+    std::cout << "Deleted: "<< q.front() << std::endl;
+    q.pop();
+    
+    showQueue(q);
+
+}
