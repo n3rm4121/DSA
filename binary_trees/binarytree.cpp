c2de115fff4c7e6afca6d883a52c5716296d825b
n3rma121 <nirmaldhakal2032@gmail.com>
2024-03-12 05:27:29 +0545

    added levelOrder traversing in binarytree.cpp

--- binary_trees/binarytree.cpp
+++ binary_trees/binarytree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 class Node{
     public:
@@ -44,19 +45,55 @@ void postOrder(Node* node){
     postOrder(node->right);
     cout << node-> data << "->";
 }
+
+void levelOrder(Node* node){
+    if(node == nullptr){
+        return;
+    }
+
+    std::queue<Node*> q;
+    q.push(node);
+
+    while(!q.empty()){
+        Node* current = q.front();
+        q.pop();
+
+        cout << current->data << " ";
+
+        if(current->left){
+            q.push(current->left);
+        }
+        if(current->right){
+            q.push(current->right);
+        }
+    }
+}
 int main() {
+
+    //                   1
+    //                  / \
+    //                 2   3
+    //                / \   \
+    //               4   5   6
    Node* root = new Node(1);
-   root->left = new Node(12);
-   root->right = new Node(9);
-   root->left->left = new Node(5);
-   root->left -> right = new Node(6);
+   root->left = new Node(2);
+   root->right = new Node(3);
+   root->left->left = new Node(4);
+   root->left -> right = new Node(5);
+   root->right->right = new Node(6);
     cout << "preOrder traversal: " << endl;
    preOrder(root);
    cout << endl;
+
    cout << "inOrder traversal: " << endl;
    inOrder(root);
    cout << endl ;
+
    cout << "postOrder: "<< endl;
    postOrder(root);
+   cout << endl;
+
+   cout << "levelOrder: "<< endl;
+   levelOrder(root);
    cout << endl;
 }
